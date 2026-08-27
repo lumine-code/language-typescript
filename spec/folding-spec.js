f0ef6b5c5f7fde93b34e2e96cd50f0ca718c7423
Sebastian Balcerowiak <asiloisad.93@gmail.com>
2026-08-27 22:42:27 +0200

    Move scoped settings to editor namespace

--- spec/folding-spec.js
+++ spec/folding-spec.js
@@ -21,7 +21,7 @@ describe("TypeScript folding (Tree-sitter)", () => {
   }
 
   beforeEach(async () => {
-    lumine.config.set("language.useTreeSitterParsers", true);
+    lumine.config.set("editor.useTreeSitterParsers", true);
 
     editor = await lumine.workspace.open();
     editor.displayLayer.reset({ foldCharacter: "…" });
